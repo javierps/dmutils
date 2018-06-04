39a5538787d08a6ac0e652df54ea3725cf44ae11
javierps <javier.perezsaez@epfl.ch>
2018-06-04 11:02:19 +0200

    first commit

---
+++ database/metadataGenerator.R
@@ -0,0 +1,13 @@
+# Title: Metadata generator
+# Description: Generates metadata as postgresql comments
+# Date: Mon Jun  4 10:51:35 2018
+# Author: Javier Perez-Saez
+
+
+
+# Preamble ---------------------------------------------------------------
+library(DBI)
+library(yaml)
+
+
+
